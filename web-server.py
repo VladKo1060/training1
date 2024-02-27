f5c14b8fb06b9fac82c240fd2cd54c65704f817b
VladKo1060 <134947972+VladKo1060@users.noreply.github.com>
2024-02-27 22:59:52 +0300

    Сделано куча всего, что не помню. Может что то не работать

--- web-server.py
+++ web-server.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
+@app.route('/')   # endpoint
+def hello():      # функция представления
 
     return f"""
     <!DOCTYPE html>
@@ -17,7 +17,11 @@ def hello():
         <h1>Тестовый сайт!!!</h1>
         <text>Это написано в теге текст</text>
         <a href={url_for("index")}>индекс</a>
-        <p><img src={{ url_for('static', filename="img.jpg" alt="Описание изображения"}}></p>
+        <a href={url_for("base")}>это база</a>
+        <a href={url_for("start")}>старт</a>
+        <a href={url_for("form")}>Форма</a>
+        <a href={url_for("login")}>лог ин</a>
+        <p><img src={url_for('static', filename="img.jpg")} alt="Описание изображения"></p>
     </ body>
     </ html>
     """
@@ -26,6 +30,40 @@ def hello():
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+
+@app.route('/start')
+def start():
+    return render_template('start.html')
+
+
+@app.route('/day-<num>')
+def day(num):
+    return render_template(f'day-{num}.html')
+
+
+@app.route('/day-15/photo-<num>')
+def photo(num):
+    return render_template(f'photo-{num}.html')
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.mathod == 'POST':
+        print(request.form)
+    return render_template(f'form.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form)
+    return render_template(f'form_login.html')
 
 
 if __name__ == '__main__':
